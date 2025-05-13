bbc4f74d993f126d19de4723bfcc78cb1a8c0637
LV <lv@mac.home>
2025-05-13 05:01:23 +0200

    strjoin refactor

--- libft/src/string/ft_strjoin.c
+++ libft/src/string/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: atbicer <atbicer@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 00:45:35 by atbicer           #+#    #+#             */
-/*   Updated: 2023/08/30 01:27:42 by atbicer          ###   ########.fr       */
+/*   Updated: 2025/05/13 05:00:50 by atbicer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*join;
 	size_t	s1_len;
 	size_t	s2_len;
-	size_t	total_len;
 
 	if (!s1 || !s2)
 		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	total_len = s1_len + s2_len;
-	join = ft_calloc(total_len + 1, sizeof(char));
+	join = ft_calloc(s1_len + s2_len + 1, sizeof(char));
 	if (!join)
 		return (NULL);
 	ft_strlcpy(join, (char *)s1, s1_len + 1);
-	ft_strlcpy(join + s1_len, (char *)s2, total_len + 1);
+	ft_strlcpy(join + s1_len, (char *)s2, s2_len + 1);
 	return (join);
 }
